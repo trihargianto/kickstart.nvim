b563f6ce808126117e51e747ded5d90908606816
Tri Hargianto <trihargianto@gmail.com>
2024-01-21 19:29:45 +0700

    mapping jj to exit insert more

--- init.lua
+++ init.lua
@@ -676,6 +676,9 @@ vim.o.shiftwidth=2
 vim.o.softtabstop=2
 vim.o.expandtab=true
 
+-- Key mappings
+vim.api.nvim_set_keymap("i", "jj", "<esc>", { noremap = true })
+
 -- APPEARANCE
 ------------------------------------------------------------------------
 
